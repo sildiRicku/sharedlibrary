69f6d53e3de186b644510cf9bb6070e9a1797f81
Klea <kleaprifti21@gmail.com>
2024-02-08 16:49:36 +0100

    commits

---
+++ src/main/java/com/example/sharedlibrary/EmailService.java
@@ -0,0 +1,27 @@
+package com.example.sharedlibrary;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+    private JavaMailSender javaMailSender;
+
+    @Autowired
+    public EmailService(JavaMailSender javaMailSender) {
+        this.javaMailSender = javaMailSender;
+    }
+
+    public void sendEmail(String recipient, String subject, String content) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(recipient);
+        message.setSubject(subject);
+        message.setText(content);
+
+        javaMailSender.send(message);
+    }
+}
+
